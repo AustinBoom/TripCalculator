3d68103a731029d8e47316b3bd35a300c5aecef1
AustinBoom <54464435+AustinBoom@users.noreply.github.com>
2021-06-23 21:10:45 -0600

    Main functionality added

--- src/GasCost.java
+++ src/GasCost.java
@@ -1,6 +1,40 @@
+import java.util.Scanner;
+
 public class GasCost {
 
-    public void FetchGasCost(){
+    private double totalGas;
 
+    public GasCost(){
+        this.totalGas = 0;
+    }
+
+    public double fetchGasCost(){
+        double miles = retrieveMiles();
+        double gasCost = retrieveGasCost();
+        double avgMPG = retrieveAvgMPG();
+
+        return calculateTotalGasCost(miles, gasCost, avgMPG);
+    }
+
+    private double retrieveMiles(){
+        Scanner mileScan = new Scanner(System.in);
+        System.out.print("Enter the total miles to be travelled:\t");
+        return mileScan.nextDouble();
+    }
+
+    private double retrieveGasCost(){
+        Scanner gasScan = new Scanner(System.in);
+        System.out.print("Enter the average price of gas:\t");
+        return gasScan.nextDouble();
+    }
+
+    private double retrieveAvgMPG(){
+        Scanner mpgScan = new Scanner(System.in);
+        System.out.print("Enter the average MPG of your vehicle:\t");
+        return mpgScan.nextDouble();
+    }
+
+    private double calculateTotalGasCost(double miles, double gasCost, double avgMPG){
+        return miles/avgMPG * gasCost;
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,11 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("henlo world");
+
+        double cost = 0;
+        GasCost gas = new GasCost();
+
+        cost = gas.fetchGasCost();
+
+        System.out.println("The trip will cost $" + String.format("%.2f", cost) + " in gas.");
     }
 }
